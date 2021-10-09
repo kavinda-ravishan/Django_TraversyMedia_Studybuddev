b239a93aa5c110f8136be3cdde80ad1739ae30a4
kavinda-ravishan <kvnravishan@outlook.com>
2021-10-09 19:54:00 +0530

    pass variables to templates

--- base/views.py
+++ base/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id':1, 'name': 'lets learn python'},
+    {'id':2, 'name': 'c++ tutorials'},
+    {'id':3, 'name': 'TensorFlow'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms':rooms})
 
 def room(request):
     return render(request, 'room.html')
