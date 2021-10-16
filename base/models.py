0b8a764ca6e70d1ad88001aedecb3a7e2c915066
kavinda-ravishan <kvnravishan@outlook.com>
2021-10-16 19:42:21 +0530

    model with ForeignKey

--- base/models.py
+++ base/models.py
@@ -1,9 +1,20 @@
+from os import name
 from django.db import models
 from django.db.models.base import Model
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import RECURSIVE_RELATIONSHIP_CONSTANT
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Room(models.Model):
-    #host = 
-    #topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True) # database field (can Empty), form field (can Empty)
     #participants =
@@ -12,3 +23,17 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE) # when room delete, delete all chiled messages
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)     
+
+    def __str__(self):
+        return self.body[0:50]
+    
+
+# python .\manage.py makemigrations
+# python .\manage.py migrate
